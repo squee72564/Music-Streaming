428293761a03643c703cf74cdbe83fc3c0103f82
squee72564 <rodriguez.al183@gmail.com>
2024-04-18 12:02:01 -0700

    Edits to static/media file directories

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,6 +20,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+    ]
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
