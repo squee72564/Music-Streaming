29085c56f4e2b3a1803b435a5e7e46e362d6f2d8
squee72564 <rodriguez.al183@gmail.com>
2024-04-17 17:34:09 -0700

    Create initial models for music representation

--- backend/music/admin.py
+++ backend/music/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Label)
+class LabelAdmin(admin.ModelAdmin):
+    list_display = ("label_name",)
+
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ("artist_name",)
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ("album_title", "label", "genre")
+
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ("song_title", "duration", "album")
+    filter_horizontal = ("artists",)
--- backend/music/models.py
+++ backend/music/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Label(models.Model):
+    label_name = models.CharField(max_length=255)
+
+
+class Artist(models.Model):
+    artist_name = models.CharField(max_length=255)
+
+
+class Album(models.Model):
+    album_title = models.CharField(max_length=255)
+    label = models.ForeignKey(Label, on_delete=models.CASCADE)
+    genre = models.CharField(max_length=255)
+
+
+class Song(models.Model):
+    song_title = models.CharField(max_length=255)
+    duration = models.DurationField()
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    artists = models.ManyToManyField(Artist)
