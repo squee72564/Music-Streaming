ad2ef143247ca37d679fc5239d0eef4f69b86edb
squee72564 <rodriguez.al183@gmail.com>
2024-04-18 13:58:11 -0700

    Create initial api list views for all albums / single album registered to a user

--- backend/music/views.py
+++ backend/music/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.exceptions import PermissionDenied
+from rest_framework import generics
+from .models import Album
+from .serializers import AlbumSerializer
 
-# Create your views here.
+
+class UserAlbumsAPIView(generics.ListAPIView):
+    serializer_class = AlbumSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user_id = self.kwargs["user_id"]
+
+        if self.request.user.is_superuser or self.request.user.id == user_id:
+            return Album.objects.filter(user_id=user_id)
+        else:
+            raise PermissionDenied("You are not allowed to access this resource.")
+
+
+class UserAlbumAPIView(generics.ListAPIView):
+    serializer_class = AlbumSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user_id = self.kwargs["user_id"]
+        album_id = self.kwargs["album_id"]
+
+        if self.request.user.is_superuser or self.request.user.id == user_id:
+            return Album.objects.filter(user_id=user_id, id=album_id)
+        else:
+            raise PermissionDenied("You are not allowed to access this resource.")
