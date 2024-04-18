246648cca7d15447af840cf50b0f388033c61cd7
squee72564 <rodriguez.al183@gmail.com>
2024-04-17 17:25:21 -0700

    Added settings for cors, initial apps, and rest_framework

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "corsheaders",
+    "music",
+    "users",
+    "streaming",
 ]
 
 MIDDLEWARE = [
@@ -47,7 +52,12 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
+
+REST_FRAMEWORK = {"DEFAULT_PERMISSION_CLASSES": ["rest_framwork.permission.AllowAny"]}
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = "backend.urls"
 
