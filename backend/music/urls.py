d14a22cef53123c9917bc938769aae094fb2ec5f
squee72564 <rodriguez.al183@gmail.com>
2024-04-20 13:55:09 -0700

    Fix model create url

--- backend/music/urls.py
+++ backend/music/urls.py
@@ -4,70 +4,66 @@ from .views import *
 urlpatterns = [
     # Albums
     path(
-        "api/user/<int:user_id>/albums/create/",
+        "api/albums/create/",
         UserAlbumsCreateAPIView.as_view(),
-        name="user_album_create_api"
+        name="user_album_create_api",
     ),
     path(
         "api/user/<int:user_id>/albums/<int:pk>/",
         UserAlbumsRetrieveUpdateDestroyAPIView.as_view(),
-        name="user_album_rud_api"
+        name="user_album_rud_api",
     ),
     path(
         "api/user/<int:user_id>/albums/",
         UserAlbumsListAPIView.as_view(),
         name="user_albums_list_api",
     ),
-
     # Songs
     path(
-        "api/user/<int:user_id>/songs/create/",
+        "api/songs/create/",
         UserSongsCreateAPIView.as_view(),
-        name="user_songs_create_api"
+        name="user_songs_create_api",
     ),
     path(
         "api/user/<int:user_id>/songs/<int:pk>/",
         UserSongsRetrieveUpdateDestroyAPIView.as_view(),
-        name="user_songs_rud_api"
+        name="user_songs_rud_api",
     ),
     path(
         "api/user/<int:user_id>/songs/",
         UserSongsListAPIView.as_view(),
         name="user_songs_list_api",
     ),
-
     # Artists
     path(
-        "api/user/<int:user_id>/artists/create/",
+        "api/artists/create/",
         UserArtistsCreateAPIView.as_view(),
-        name="user_artsts_create_api"
+        name="user_artsts_create_api",
     ),
     path(
         "api/user/<int:user_id>/artists/<int:pk>/",
         UserArtistsRetrieveUpdateDestroyAPIView.as_view(),
-        name="user_artsts_rud_api"
+        name="user_artsts_rud_api",
     ),
     path(
         "api/user/<int:user_id>/artists/",
         UserArtistsListAPIView.as_view(),
         name="user_artists_list_api",
     ),
-
     # Labels
     path(
-        "api/user/<int:user_id>/labels/create/",
+        "api/labels/create/",
         UserLabelsCreateAPIView.as_view(),
-        name="user_labels_create_api"
+        name="user_labels_create_api",
     ),
     path(
         "api/user/<int:user_id>/labels/<int:pk>/",
         UserLabelsRetrieveUpdateDestroyAPIView.as_view(),
-        name="user_labels_rud_api"
+        name="user_labels_rud_api",
     ),
     path(
         "api/user/<int:user_id>/labels/",
         UserLabelsListAPIView.as_view(),
         name="user_labels_list_api",
     ),
-
 ]
