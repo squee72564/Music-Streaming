eee7f0b3d53002c0469c8f9be16dfd2e733af52b
squee72564 <rodriguez.al183@gmail.com>
2024-04-19 18:17:56 -0700

    Add ListAPIView for user songs

--- backend/music/views.py
+++ backend/music/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics, permissions
 from rest_framework.exceptions import PermissionDenied
 from rest_framework import generics
 from .models import Album
-from .serializers import AlbumSerializer
+from .serializers import *
 
 
 class UserAlbumsAPIView(generics.ListAPIView):
@@ -18,15 +18,14 @@ class UserAlbumsAPIView(generics.ListAPIView):
             raise PermissionDenied("You are not allowed to access this resource.")
 
 
-class UserAlbumAPIView(generics.ListAPIView):
-    serializer_class = AlbumSerializer
+class UserSongsAPIView(generics.ListAPIView):
+    serializer_class = SongSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         user_id = self.kwargs["user_id"]
-        album_id = self.kwargs["album_id"]
 
         if self.request.user.is_superuser or self.request.user.id == user_id:
-            return Album.objects.filter(user_id=user_id, id=album_id)
+            return Song.objects.filter(user_id=user_id)
         else:
             raise PermissionDenied("You are not allowed to access this resource.")
