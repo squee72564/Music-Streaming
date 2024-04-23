3809b9e0b34ece569a334ff252bcf472682d3ab5
squee72564 <rodriguez.al183@gmail.com>
2024-04-23 12:43:31 -0700

    Setting up webpack to use babel for our .js / .jsx files
    Setting up webpack to use Babel for these files will allow
    our modern javascript and jsx files via React to be compiled
    to browser compatible javascript in our static directory

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -6,4 +6,14 @@ module.exports = {
         filename: 'index-bundle.js',
         path: path.resolve(__dirname, './static'),
     },
+    module : {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: { presets: ['@babel/preset-env', '@babel/preset-react'] },
+            },
+        ]
+    }
 }
