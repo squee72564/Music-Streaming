827ec931817d94026b55e72dd3544db121e5f4f5
squee72564 <rodriguez.al183@gmail.com>
2024-04-23 12:23:15 -0700

    Add test url
    The test url renders the base template as a view.
    This is a integration test for our frontend pipeline
    using webpack to bundle our npm packages and js files
    as static files that can be served from our django server
    and rendered using templates

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,8 +19,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="base_template.html")),
     path("admin/", admin.site.urls),
     path("music/", include("music.urls")),
 ]
