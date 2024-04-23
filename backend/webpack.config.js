ff75042fe8b569979fa4ad11709f382746923310
squee72564 <rodriguez.al183@gmail.com>
2024-04-23 13:37:45 -0700

    Update parameters for the webpack config

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path')
 
 module.exports = {
-    entry: './assets/index.js',
+    entry: './frontend/index.js',
     output: {
-        filename: 'index-bundle.js',
+        filename: 'js-bundle.js',
         path: path.resolve(__dirname, './static'),
     },
     module : {
