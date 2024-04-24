edbd64038d881fa0f09a5bea003adabc050f5caa
squee72564 <rodriguez.al183@gmail.com>
2024-04-23 19:21:40 -0700

    added accounts/ prefix to home urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,7 +21,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("home.urls")),
+    path("accounts/", include("home.urls")),
     path("admin/", admin.site.urls),
     path("music/", include("music.urls")),
 ]
