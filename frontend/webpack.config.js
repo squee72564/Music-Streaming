4273bcd20c8947ef469b22c0ed5dc952c22d6092
squee72564 <rodriguez.al183@gmail.com>
2024-04-23 17:35:12 -0700

    Update webpack and babel config files

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-    entry: './assets/index.js',
+    entry: './src/index.jsx',
     output: {
         filename: 'js-bundle.js',
         path: path.resolve(__dirname, '../backend/static/'),
@@ -11,9 +11,10 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader',
-                options: { presets: ['@babel/preset-env', '@babel/preset-react'] },
+                use: {
+                    loader: 'babel-loader',
+                },
             },
-        ]
-    }
+        ],
+    },
 }
