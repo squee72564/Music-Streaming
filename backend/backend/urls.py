ad76073c98cceb01856c2de1a02dff61fc3c79d3
squee72564 <rodriguez.al183@gmail.com>
2024-04-24 12:03:02 -0700

    Added home page as base url and reordered url configuration

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,10 +19,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from home.views import HomePageView
 
 urlpatterns = [
-    path("accounts/", include("home.urls")),
     path("admin/", admin.site.urls),
+    path("", HomePageView.as_view(), name="home"),
+    path("accounts/", include("home.urls")),
     path("music/", include("music.urls")),
 ]
 
