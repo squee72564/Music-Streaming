92d6a3eed2e59504652b032d3ce62e89757e6b63
squee72564 <rodriguez.al183@gmail.com>
2024-04-23 19:40:47 -0700

    Added basename to browser router and edited route paths
    This may not be idea later as the basename may not always be the same

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
 // App.js
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TestPage1 from './pages/TestPage1';
 import TestPage2 from './pages/TestPage2';
 import TestPage3 from './pages/TestPage3';
 
 const App = () => {
  return (
-    <>
+    <BrowserRouter basename="/music/test-react-page"> {/* Specify the basename here */}
        <Routes>
-         <Route path="/music/test1" element={<TestPage1 />} />
-         <Route path="/music/test2" element={<TestPage2 />} />
-         <Route path="/music/test3" element={<TestPage3 />} />
+         <Route path="/1" element={<TestPage1 />} />
+         <Route path="/2" element={<TestPage2 />} />
+         <Route path="/3" element={<TestPage3 />} />
        </Routes>
-    </>
+    </BrowserRouter>
  );
 };
 
