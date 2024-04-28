4c5f5a21116f04b1b2f05e5e45bacc716ab74440
squee72564 <rodriguez.al183@gmail.com>
2024-04-27 21:19:03 -0700

    Add Form class that extends base AuthenticationForm for LoginView. Add logout function to sign up view

--- backend/home/views.py
+++ backend/home/views.py
@@ -2,10 +2,35 @@ from django.views.generic import CreateView, TemplateView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout as auth_logout
+
+
+class LoginAuthenticationForm(AuthenticationForm):
+    """
+    This class extends the basic AuthenticationForm class used in the LoginView view.
+    We can override some of the fields for the form widgets if needed.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["username"].widget.attrs.update(
+            {
+                "placeholder": "Enter username",
+                "style": "padding: 8px",
+            }
+        )
+        self.fields["password"].widget.attrs.update(
+            {
+                "placeholder": "Enter password",
+                "style": "padding: 8px",
+            }
+        )
 
 
 class LoginFormView(LoginView):
     template_name = "home/login.html"
+    form_class = LoginAuthenticationForm
 
 
 class LogoutInterfaceView(LoginRequiredMixin, LogoutView):
@@ -20,6 +45,10 @@ class SignupCreateView(CreateView):
     form_class = UserCreationForm
     template_name = "home/signup.html"
     success_url = "/accounts/login/"
+
+    def form_valid(self, form):
+        auth_logout(self.request)
+        return super().form_valid(form)
 
 
 class HomePageView(TemplateView):
