3313e0f6cdc27328dc26120008f2e96abbe092e8
Alex Rodriguez <rodriguez.al183@gmail.com>
2024-05-22 17:27:05 -0700

    Add Album page to route

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import MusicCollection from './pages/MusicCollection';
+import AlbumPage from './pages/AlbumPage';
 import ErrorPage from './pages/ErrorPage';
 
 export const router = createBrowserRouter([
@@ -7,5 +8,10 @@ export const router = createBrowserRouter([
       path: "/music/collection/",
       element: <MusicCollection />,
       errorElement: <ErrorPage />,
+    },
+    {
+      path: '/music/collection/:albumId/',
+      element: <AlbumPage />,
+      errorElement: <ErrorPage />,    
     },
   ]);
