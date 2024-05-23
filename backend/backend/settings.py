12786a3e0e87d9db77991a2ce26ace277461b8a9
Alex Rodriguez <rodriguez.al183@gmail.com>
2024-05-22 20:25:18 -0700

    Added pagination for apis

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,6 +62,8 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 8,
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
