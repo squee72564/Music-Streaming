30f1e84af2b930f7dbd6f9b2ce283a0a406216e1
squee72564 <rodriguez.al183@gmail.com>
2024-05-27 19:06:33 -0700

    Update create method for Album Serializer

--- backend/music/serializers.py
+++ backend/music/serializers.py
@@ -48,3 +48,36 @@ class AlbumNestedSerializer(serializers.ModelSerializer):
     class Meta:
         model = Album
         exclude = ["user"]
+
+    def create(self, validated_data):
+        label_data = validated_data.pop("label")
+        genres_data = validated_data.pop("genres")
+        songs_data = validated_data.pop("songs")
+
+        label, _ = Label.objects.get_or_create(
+            user=validated_data["user"], **label_data
+        )
+
+        album, _ = Album.objects.get_or_create(label=label, **validated_data)
+
+        for genre_data in genres_data:
+            genre, _ = Genre.objects.get_or_create(
+                user=validated_data["user"], **genre_data
+            )
+            album.genres.add(genre)
+
+        for song_data in songs_data:
+            artists_data = song_data.pop("artists")
+            song = Song.objects.create(
+                user=validated_data["user"], album=album, **song_data
+            )
+            song.artists.add(
+                *[
+                    Artist.objects.get_or_create(
+                        user=validated_data["user"], **artist_data
+                    )[0]
+                    for artist_data in artists_data
+                ]
+            )
+
+        return album
