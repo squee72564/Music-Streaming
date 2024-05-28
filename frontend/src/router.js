685f8506dfd07893b62368fc209e70061827a3e5
squee72564 <rodriguez.al183@gmail.com>
2024-05-27 19:09:44 -0700

    Add route for Album Creation Page to react router

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import MusicCollection from './pages/MusicCollection';
 import AlbumPage from './pages/AlbumPage';
 import ErrorPage from './pages/ErrorPage';
+import AlbumCreationPage from './pages/AlbumCreationPage';
 
 export const router = createBrowserRouter([
     {
@@ -12,6 +13,11 @@ export const router = createBrowserRouter([
     {
       path: '/music/collection/:albumId/',
       element: <AlbumPage />,
+      errorElement: <ErrorPage />,    
+    },
+    {
+      path: '/music/create/',
+      element: <AlbumCreationPage />,
       errorElement: <ErrorPage />,    
     },
   ]);
