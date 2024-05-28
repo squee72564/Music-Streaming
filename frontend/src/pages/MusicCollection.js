8d000dc2c5130be33220716d040f5a33ff92a31a
squee72564 <rodriguez.al183@gmail.com>
2024-05-27 19:09:11 -0700

    Formatting edits

--- frontend/src/pages/MusicCollection.js
+++ frontend/src/pages/MusicCollection.js
@@ -75,42 +75,51 @@ const MusicCollection = () => {
     }
   }
 
-
   return (
-    <div className='rounded flex flex-col justify-center m-5'>
+    <div className='flex flex-col justify-center m-5'>
       <h1 className='text-xl font-bold m-5'>Welcome, {username}!</h1>
-      <div className='rounded-b bg-gray-200 text-center space-y-8 w-full'>
+      <div className='text-center space-y-8'>
         <h1 className='text-xl font-bold m-3'>Your Albums</h1>
         <div className='space-x-10'>
           <button className='rounded bg-gray-400 px-4' onClick={() => fetchPageData(prevPage)}>Prev</button>
           <button className='rounded bg-gray-400 px-4' onClick={() => fetchPageData(nextPage)}>Next</button>
         </div>
-        <div id='Albums' className='flex justify-center space-x-10 m-5 min-h-48'>
+        <div id='Albums' className='flex justify-center overflow-x-auto space-x-10 m-5 min-h-48'>
           {albums && albums.map((album) => (
             <div key={album.id} onClick={()=>handleAlbumClick(album.id)} style={{ cursor: 'pointer' }}>
-              <h2>{album.album_title}</h2>
+              <p className='font-medium'>{album.album_title}</p>
               <img src={album.image} className="w-32 h-32" alt={album.album_title}></img>
               <p>{album.genre}</p>
             </div>
           ))}
         </div>
         <h1 className='text-xl font-bold m-3'>Filter by...</h1>
-        <div className='flex flex-row justify-around'>
-          <div className='flex flex-col'>
+        <div className='flex flex-row justify-center space-x-10'>
+          <div className='flex flex-col border-2 border-black'>
             <h2 className='font-bold'>Label</h2>
             <ul className='rounded overflow-y-auto bg-white h-24 '>
               <li className='m-2' onClick={() => fetchPageData('http://127.0.0.1:8000/music/api/albums/')}>All labels</li>
               {labels && labels.map((label) => (
-                <li key={label.id} className='m-2' onClick={() => fetchPageData(`http://127.0.0.1:8000/music/api/albums/?label=${label.id}`)}>{label.label_name}</li>
+                <li key={label.id}
+                    className='m-2'
+                    onClick={() => fetchPageData(`http://127.0.0.1:8000/music/api/albums/?label=${label.id}`)}
+                >
+                  {label.label_name}
+                </li>
               ))}
             </ul>
           </div>
-          <div className='flex flex-col'>
+          <div className='flex flex-col border-2 border-black'>
             <h2 className='font-bold'>Genre</h2>
             <ul className='rounded overflow-y-auto bg-white h-24 '>
               <li className='m-2' onClick={() => fetchPageData('http://127.0.0.1:8000/music/api/albums/')}>All Genres</li>
               {genres && genres.map((genre) => (
-                <li key={genre.id} className='m-2' onClick={() => fetchPageData(`http://127.0.0.1:8000/music/api/albums/?genres=${genre.id}`)}>{genre.genre_name}</li>
+                <li key={genre.id}
+                    className='m-2'
+                    onClick={() => fetchPageData(`http://127.0.0.1:8000/music/api/albums/?genres=${genre.id}`)}
+                >
+                  {genre.genre_name}
+                </li>
               ))}
             </ul>
           </div>
