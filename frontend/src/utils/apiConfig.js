ba1b76b4b303be43315bcfb5291d06027a2fe861
Alex Rodriguez <rodriguez.al183@gmail.com>
2024-06-01 13:12:30 -0700

    Renamed services folder to utils, added apiConfig.js for all backend links

---
+++ frontend/src/utils/apiConfig.js
@@ -0,0 +1,9 @@
+const BASE_URL = "http://127.0.0.1:8000";
+const API_PATH = "/music/api/";
+
+export const API_URLS = {
+  ALBUMS: `${BASE_URL}${API_PATH}albums/`,
+  LABELS: `${BASE_URL}${API_PATH}labels/`,
+  GENRES: `${BASE_URL}${API_PATH}genres/`,
+  ARTISTS: `${BASE_URL}${API_PATH}artists/`,
+};
