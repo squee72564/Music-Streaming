a627b49d06d1dfbe9c20540ee3f31b5fb6917d04
squee72564 <rodriguez.al183@gmail.com>
2024-07-06 15:40:38 -0700

    Added padding for each album component

--- app/frontend/src/components/Album.js
+++ app/frontend/src/components/Album.js
@@ -5,7 +5,7 @@ const Album = ({ album, handleAlbumClick }) => {
     <div
       key={album.id}
       onClick={() => handleAlbumClick(album.id)}
-      className="transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300"
+      className="p-5 transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300"
       style={{ cursor: "pointer" }}
     >
       <p className="font-medium">{album.album_title}</p>
